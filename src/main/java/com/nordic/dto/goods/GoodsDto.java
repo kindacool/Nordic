0030dd210b6bb1e726d56e4fb22dc5bd2fe7cb7a
laker99 <moonvie831@gmail.com>
2022-12-20 20:34:36 +0900

    Spring Validation 사용해서 굿즈 등록 및 수정 시 유효성 검사(길이) 추

--- src/main/java/com/nordic/dto/goods/GoodsDto.java
+++ src/main/java/com/nordic/dto/goods/GoodsDto.java
@@ -3,10 +3,12 @@ package com.nordic.dto.goods;
 import java.sql.Timestamp;
 import java.util.List;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import org.apache.ibatis.type.Alias;
 
@@ -19,8 +21,10 @@ import lombok.Data;
 public class GoodsDto {
 	private int goods_no;
 	@NotBlank(message="굿즈명을 입력해주세요")
+	@Size(max=100)
 	private String goods_name;
 	@Positive(message="포인트는 0 보다 커야합니다")
+	@Max(value=100000, message="포인트는 100,000 보다 작아야합니다")
 	private int point;
 	@NotBlank(message="상세설명을 입력해주세요")
 	private String goods_desc;
@@ -30,6 +34,7 @@ public class GoodsDto {
 	private String image4;
 	private String image5;
 	private char use_yn;
+	@Size(max=1000)
 	private String remark;
 	private String create_member;
 	@JsonFormat(pattern = "yyyy-MM-dd a HH:mm:ss", timezone = "Asia/Seoul")
